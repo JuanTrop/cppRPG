4fa85e7cbdda050a2ade715f53e71722cf6c1635
Kevin Rangel <Krangeljimenez@hotmail.com>
2019-05-06 19:07:32 -0500

    Optimización

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,13 @@
 #include"Esctructura.hpp"
 int main(int argc , char *argv[]){
     inicio();
-    //Permitir sobreposición
-    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
-    SDL_SetRenderDrawColor(renderer,piskel.R,piskel.G,piskel.B,piskel.A);
-    SDL_RenderClear(renderer);
     cargarTexturas();
     draw();
     SDL_RenderPresent(renderer);
-    while(true){if(SDL_PollEvent(&event)&&event.type==SDL_QUIT){break;}}
+    while(true){
+        if(SDL_PollEvent(&event)&&event.type==SDL_QUIT){
+            break;
+        }
+    }
     return close();
 }
